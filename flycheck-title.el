4686fa30069f6f3d996bd63978b8fd47f534df24
Wilfred Hughes <me@wilfred.me.uk>
2016-11-13 01:16:25 +0000

    Better commentary

--- flycheck-title.el
+++ flycheck-title.el
@@ -21,7 +21,19 @@
 
 ;;; Commentary:
 
-;; See https://github.com/Wilfred/flycheck-frame-title for docs.
+;; flycheck-title lets you view flycheck messages in the frame title,
+;; keeping the minibuffer free for other things.
+
+;; This is particularly useful if you're using your minibuffer for
+;; eldoc, and using pop-ups for completion.
+
+;; To install, simply add to your configuration:
+
+;; (with-eval-after-load 'flycheck
+;;   (flycheck-title-mode))
+
+;; See https://github.com/Wilfred/flycheck-frame-title for full docs
+;; and gratuitous screenshots.
 
 ;;; Code:
 
